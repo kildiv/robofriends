5ad04d61fa831cecd50337f29b5759f395365618
AlphaHyper <kildonveseli1@gmail.com>
2020-04-24 00:15:36 +0200

    Updates

---
+++ precache-manifest.fb34a395d5db2c57200c35d6c6ae93b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49973b416306ee1b9a66d61ad41e208d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b900f8f2b8a2ae1dd3f2",
+    "url": "/robofriends/static/css/main.b37c6240.chunk.css"
+  },
+  {
+    "revision": "e48d547c85d55eda1a64",
+    "url": "/robofriends/static/js/2.2be6da75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2be6da75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b900f8f2b8a2ae1dd3f2",
+    "url": "/robofriends/static/js/main.a95d173f.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
