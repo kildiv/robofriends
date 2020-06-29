2c8a2b1ae9706fa49455cfe62cd38e4730c5352f
Kildon Veseli <kildonveseli1@gmail.com>
2020-06-30 00:12:13 +0200

    handling state management with redux

--- src/container/App.js
+++ src/container/App.js
@@ -5,14 +5,26 @@ import Scroll from '../components/Scroll';
 import Footer from '../components/Footer';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
+import { connect } from 'react-redux';
+import { setSearchField } from '../action';
 
+const mapStateToProps = state => {
+  return{
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
 
 class App extends Component  {
   constructor () {
     super()
     this.state = {
       robots: [],
-      searchField: ''
     }
   }
 
@@ -22,22 +34,18 @@ class App extends Component  {
     .then(users => this.setState({ robots: users}))
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value })
-  }
-
   render() {
-    const { robots, searchField } = this.state
+    const { robots } = this.state
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     }) 
-
      return !robots.length 
       ? <h1 className='f1 tc'>Loading...</h1>
       :(
       <div className='tc'>
         <h1 className='f1'>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots}/>
@@ -49,4 +57,4 @@ class App extends Component  {
     }
   };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps) (App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
+import { createLogger } from 'redux-logger'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+ReactDOM.render(
+        <Provider store = {store}>
+            <App/>
+        </Provider>
+        , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
